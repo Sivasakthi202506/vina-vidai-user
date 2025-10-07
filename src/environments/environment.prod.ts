3cc569752330011ad861e54d68951750fdc5d2d2
lilly pakiyam <lilypackiyam@gmail.com>
2025-10-07 18:39:42 +0530

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebase: {
+    apiKey: 'AIzaSyB8SEsMbunAn5PxC5WhLpBZSsAA2AhgV64',
+    authDomain: 'myproject-4f301.firebaseapp.com',
+    projectId: 'myproject-4f301',
+    storageBucket: 'myproject-4f301.appspot.com',
+    messagingSenderId: '572265693753',
+    appId: '1:572265693753:web:52fc007ce08c77414b43e8'  // ✅ fixed key name and value
+  }
 };
