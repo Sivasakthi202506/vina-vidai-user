79847b43171d0d215a00412334a235aea38d7759
Divyak2709 <kdivya2k20@gmail.com>
2025-10-12 18:49:57 +0530

    origin

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.quizapp',
+  appName: 'Quiz App',
+  webDir: 'www'
+};
+
+export default config;
